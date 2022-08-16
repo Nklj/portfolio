83ee15727fdb996f5ec502ec67da6d5f7dd87fe9
Nklj <vlkvnklj@gmail.com>
2022-08-16 16:37:24 +0300

    add 2 vue projects

--- src/data/worksData.js
+++ src/data/worksData.js
@@ -1,5 +1,27 @@
 const worksData = [
   {
+    name: "Memory Game",
+    title: "Memory Game",
+    img: "memory-game.png",
+    path: 5,
+    stack: ["VUE", "lodash", ],
+    descr:
+      "Игра на тренеровку памяти",
+    link: " https://nklj.github.io/vue-memory-game/",
+    code: "https://github.com/Nklj/vue-memory-game",
+  },
+  {
+    name: "Todo App",
+    title: "Todo App",
+    img: "vue-test-task.png",
+    path: 6,
+    stack: ["VUE", "VUEX", "vue-router", "materialize-css","SCSS"],
+    descr:
+      "Vue CRUD TODO App",
+    link: "https://nklj.github.io/Vue-todo-app/",
+    code: "https://github.com/Nklj/Vue-todo-app",
+  },
+  {
     name: "BrownMove",
     title: "BrownMove",
     img: "brownMove.png",
